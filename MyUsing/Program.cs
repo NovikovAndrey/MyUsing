8317dbd2a2385ec9868dacaa18f0ddf01ae8d2bd
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-13 14:08:24 +0300

    Add project files.

---
+++ MyUsing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
